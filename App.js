8be36ca9b426f3c5cd9f7598b595688dce677731
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2022-01-09 13:18:21 -0500

    Picking image

--- App.js
+++ App.js
@@ -1,8 +1,39 @@
+import * as ImagePicker from 'expo-image-picker'
 import { StatusBar } from 'expo-status-bar'
+import React from 'react'
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import logo from './assets/logo.png'
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = React.useState(null)
+
+  const openImagePickerAsync = async () => {
+    const permissionResult = await ImagePicker.requestCameraPermissionsAsync()
+    if (permissionResult.granted === false) {
+      alert('Permission to access camera roll is required')
+      return
+    }
+
+    const pickerResult = await ImagePicker.launchImageLibraryAsync()
+
+    if (pickerResult.cancelled === true) {
+      return
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri })
+  }
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+      </View>
+    )
+  }
+
   return (
     <View style={styles.container}>
       <Image source={logo} style={styles.logo} />
@@ -11,10 +42,7 @@ export default function App() {
         below!
       </Text>
 
-      <TouchableOpacity
-        onPress={() => alert('hello world!')}
-        style={styles.button}
-      >
+      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
         <Text style={styles.buttonText}>Pick a photo</Text>
       </TouchableOpacity>
       <StatusBar style='auto' />
@@ -48,5 +76,10 @@ const styles = StyleSheet.create({
   buttonText: {
     fontSize: 20,
     color: '#fff',
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: 'contain',
   },
 })
